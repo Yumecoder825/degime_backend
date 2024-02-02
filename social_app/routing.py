da499b29f3da82e2bbcab0ee433cf6c6e5652ea0
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-02 11:23:35 +0100

    Backend Update

--- social_app/routing.py
+++
@@ -1,6 +0,0 @@
-from .consumers import TextRoomConsumer
-from django.urls.conf import re_path
-
-websocket_urlpatterns = [
-    re_path(r'^chat/(?P<room_name>[^/]+)/(?P<token>[^/]+)/(?P<username>[^/]+)$', TextRoomConsumer.as_asgi()),
-]
