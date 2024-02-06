e960eed3a12509d697b17851f537928d173d3b55
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-06 08:12:08 +0100

    123

--- home/ubuntu/degime-backend/Degime_backend/settings.py
+++ home/ubuntu/degime-backend/Degime_backend/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = './home/ubuntu/degime-backend/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
