52f7f4199a7e4e52fd0fea3ee13cb2b0aa5eb1f7
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-11 07:37:36 +0100

    123

--- home/ubuntu/degime-backend/chat_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from chat_app.models import (
-    ChatRoom, ChatMessage
-    )
-# Register your models here.
-
-admin.site.register(ChatRoom)
-admin.site.register(ChatMessage)
