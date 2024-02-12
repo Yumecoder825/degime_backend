69ebff9e879732134edf5dd2348a841e5d602141
H.Morimoto <morimoto@colsystem.biz>
2024-02-12 19:03:40 +0900

    Revert "123"
    This reverts commit 52f7f4199a7e4e52fd0fea3ee13cb2b0aa5eb1f7.

---
+++ home/ubuntu/degime-backend/Degime_backend/urls.py
@@ -0,0 +1,37 @@
+"""Degime_backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from user_app.views import CheckAPIView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('api/check/', CheckAPIView.as_view(), name='Backend API Check'),
+    
+    path('social/', include('social_app.urls')),
+    
+    path('chat/', include('chat_app.urls')),
+    
+    path('account/', include('user_app.urls')),
+    
+    path('docs/', include_docs_urls(title='DEGIME_API')),
+    path('schema/', get_schema_view(
+        title="DEGIME_API", description="Guide for the CRUD operations",
+        version="1.0.0"), name='OpenApi_Schema')
+]
