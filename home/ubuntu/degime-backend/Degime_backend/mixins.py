8d02625b338d2ded3f0b17e4a4d18a5aaeef4f54
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-13 09:26:02 +0100

    123

--- home/ubuntu/degime-backend/Degime_backend/mixins.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-class AppAuthPermMixin():
-    authentication_classes = [SessionAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
