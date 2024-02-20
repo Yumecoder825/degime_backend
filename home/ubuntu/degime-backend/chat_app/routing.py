87fb1e01fc195eb381ca692a99c8e4e3ccda4486
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-20 23:04:18 +1000

    update

--- home/ubuntu/degime-backend/chat_app/routing.py
+++ home/ubuntu/degime-backend/chat_app/routing.py
@@ -2,6 +2,6 @@ from chat_app.consumers import TextRoomConsumer, PersonalConsumer
 from django.urls.conf import re_path
 
 websocket_urlpatterns = [
-    re_path(r'^chat/(?P<room_name>[^/]+)/(?P<token>[^/]+)/(?P<username>[^/]+)$', TextRoomConsumer.as_asgi()),
-    re_path(r'^personal_chat/(?P<room_name>[^/]+)/(?P<token>[^/]+)/(?P<username>[^/]+)$', PersonalConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[^/]+)/(?P<token>[^/]+)/(?P<username>[^/]+)$', TextRoomConsumer.as_asgi()),
+    re_path(r'^ws/personal_chat/(?P<room_name>[^/]+)/(?P<token>[^/]+)/(?P<username>[^/]+)$', PersonalConsumer.as_asgi()),
 ]
