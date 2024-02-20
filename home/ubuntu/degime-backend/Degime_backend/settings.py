078a592e293b09656f49357713c15e3c36f2f053
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-21 06:16:53 +1000

    update

--- home/ubuntu/degime-backend/Degime_backend/settings.py
+++ home/ubuntu/degime-backend/Degime_backend/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'Degime_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/ubuntu/degime-backend/user_app/utils.py
+++ home/ubuntu/degime-backend/user_app/utils.py
@@ -2,15 +2,21 @@ import random
 import string
 from django.core.mail import send_mail
 from django.conf import settings
+import smtplib
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 def generate_vcode(length=6):
     characters = string.digits
     vcode = ''.join(random.choice(characters) for _ in range(length))
     return vcode
 
+
 def send_vcode_email(email, vcode):
     subject = 'Email Verification'
-    message = f'Verify Code is: {vcode}'
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = [email]
-    send_mail(subject, message, from_email, recipient_list)
+    context = {'vcode': vcode}
+    html_content = render_to_string('email/verification_email.html', context)
+    email = EmailMessage(subject, html_content, settings.EMAIL_HOST_USER, [email])
+    email.content_subtype = "html"
+    email.send()
+    
