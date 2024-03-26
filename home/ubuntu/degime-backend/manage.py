1893f5b4a3f9c1fa8630f76685daef5cfd1c25fb
unknown <yasuhiroyamamoto515@gmail.com>
2024-03-26 22:57:34 +0900

    Revert "test code for committing rollback."
    This reverts commit 2f7962a7ba0318d41306c44d76f2e2fc56d1c08f.

--- home/ubuntu/degime-backend/manage.py
+++ home/ubuntu/degime-backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks.   test rollback"""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Degime_backend.settings')
     try:
         from django.core.management import execute_from_command_line
